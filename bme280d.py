4b19a2929dbbee324f61a5868195fcedddb85732
user <spacecobra@mail.ru>
2020-01-05 16:10:01 +0300

    init

---
+++ bme280d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from Adafruit_BME280 import *
+
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8, spi_bus=0, spi_dev=0)
+
+degrees = sensor.read_temperature()
+pascals = sensor.read_pressure()
+hectopascals = pascals / 100
+humidity = sensor.read_humidity()
+
+print('Temp      = {0:0.3f} deg C'.format(degrees))
+print('Pressure  = {0:0.2f} hPa'.format(hectopascals))
+print('Humidity  = {0:0.2f} %'.format(humidity))
